3463c77965408f1e98591f19db019943207aea87
= <=>
2023-12-12 23:50:41 +0100

    synchronization done

--- lista_de_libros/src/App.js
+++ lista_de_libros/src/App.js
@@ -6,34 +6,64 @@ import "./App.css";
 
 function App() {
   const { library } = listBooks;
-  const [readingList, SetReadingList] = useState([]);
-  const [availableList, SetAvailableList] = useState(library);
+  const [readingList, setReadingList] = useState([]);
+  const [availableList, setAvailableList] = useState(library);
 
   useEffect(() => {
     const getAvailableList = JSON.parse(localStorage.getItem("availableList"));
     const getReadingList = JSON.parse(localStorage.getItem("readingList"));
 
-    if (getAvailableList || getReadingList) {
-      SetAvailableList((prevAvailableList) => getAvailableList || library);
-      SetReadingList((prevReadingList) => getReadingList || []);
-    }
-    return;
+    setAvailableList(getAvailableList || library);
+    setReadingList(getReadingList || []);
   }, []);
+
+  /* Para guardar y mostrar los cambios en 2 pentañas con la misma url, utilizando las varaible almacenadas en localStorage */
+  useEffect(() => {
+    const handleAvailableListChange = (event) => {
+      if (event.key === "availableList") {
+        const updatedAvailableList = JSON.parse(event.newValue);
+        setAvailableList(updatedAvailableList);
+      }
+    };
+
+    const handleReadingListChange = (event) => {
+      if (event.key === "readingList") {
+        const updatedReadingList = JSON.parse(event.newValue);
+        setReadingList(updatedReadingList || []); // Inicializar si es nulo
+      }
+    };
+
+    // Agregar el escuchador de eventos para "availableList"
+    window.addEventListener("storage", handleAvailableListChange);
+
+    // Agregar el escuchador de eventos para "readingList"
+    window.addEventListener("storage", handleReadingListChange);
+
+    // Limpiar los escuchadores cuando el componente se desmonta
+    return () => {
+      window.removeEventListener("storage", handleAvailableListChange);
+      window.removeEventListener("storage", handleReadingListChange);
+    };
+  }, [setAvailableList, setReadingList]);
+
+  useEffect(() => {
+    localStorage.setItem("availableList", JSON.stringify(availableList));
+  }, [availableList]);
 
   return (
     <div className="container">
       <AvailableBooks
-        SetReadingList={SetReadingList}
+        setReadingList={setReadingList}
         readingList={readingList}
         availableList={availableList}
-        SetAvailableList={SetAvailableList}
+        setAvailableList={setAvailableList}
       />
-      {readingList.length > 0 && (
+      {readingList?.length > 0 && (
         <ReadingList
           readingList={readingList}
-          SetReadingList={SetReadingList}
+          setReadingList={setReadingList}
           availableList={availableList}
-          SetAvailableList={SetAvailableList}
+          setAvailableList={setAvailableList}
         />
       )}
     </div>
