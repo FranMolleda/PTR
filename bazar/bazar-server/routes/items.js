a859e4dcedd0d57f9e8a6f83b40eeb6e72941fe8
= <=>
2023-12-20 22:59:44 +0100

    Id route done

--- bazar/bazar-server/routes/items.js
+++ bazar/bazar-server/routes/items.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
-    getData
+    getData,
+    getItem
 } from '../controllers/items.js';
 
 const router = express.Router();
@@ -9,5 +10,6 @@ const path = '/item';
 
 // Pasa la variable products al controlador a través de req (rep.products)
 router.get(`${path}`, (req, res) => getData(req, res));
+router.get(`${path}/:id`, (req, res) => getItem(req, res));
 
 export default router;
